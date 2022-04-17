1bcce883df56367df2157eee1ea79dbf5725d1a5
ainunseptian <ainunseptian@kali.ainunseptian>
2022-04-17 00:53:34 -0400

    first commit

---
+++ build/static/js/main.d1e3695d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(3),r=c.n(n),s=(c(8),c(0));var i=function(){return Object(s.jsx)("h2",{children:"Expense Item!"})};var o=function(){return Object(s.jsxs)("div",{children:[Object(s.jsx)("h2",{children:"Let's get started!"}),Object(s.jsx)(i,{})]})};r.a.render(Object(s.jsx)(o,{}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.d1e3695d.chunk.js.map
